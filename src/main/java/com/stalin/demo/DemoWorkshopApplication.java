8c8c80dcff8bd67be07b27b95dfed344427bf129
Stalin <stalin.lenin@gmail.com>
2021-11-20 13:02:38 +0530

    Merge 5

--- src/main/java/com/stalin/demo/DemoWorkshopApplication.java
+++ src/main/java/com/stalin/demo/DemoWorkshopApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoWorkshopApplication {
 // Test 1
-   // test
+   
 	
 	public static void main(String[] args) {
 		// Sample comment
