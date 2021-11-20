95dd3774a7b428bb4f8229dba11bf0ce542b6805
Stalin <stalin.lenin@gmail.com>
2021-11-20 12:52:23 +0530

    merge commit 1

--- src/main/java/com/stalin/demo/DemoWorkshopApplication.java
+++ src/main/java/com/stalin/demo/DemoWorkshopApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoWorkshopApplication {
 // Test 1
    // test
+	// Test
 	public static void main(String[] args) {
 		// Sample comment
 		SpringApplication.run(DemoWorkshopApplication.class, args);
