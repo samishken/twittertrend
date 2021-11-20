a61973239148c97a60a5db8cba35be2647a01dc1
Stalin <stalin.lenin@gmail.com>
2021-11-20 13:10:22 +0530

    Master merge 8

--- src/main/java/com/stalin/demo/DemoWorkshopApplication.java
+++ src/main/java/com/stalin/demo/DemoWorkshopApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoWorkshopApplication
 	
 	public static void main(String[] args) {
-		// Sample comment
+	
 		SpringApplication.run(DemoWorkshopApplication.class, args);
 	}
 
