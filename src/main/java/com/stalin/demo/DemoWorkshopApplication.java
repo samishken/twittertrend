1b924bd7fd89a5fc5f7a1d07ee038630640b02ab
Stalin Mahapatra <stalinmahapatra@Stalins-MacBook-Pro.local>
2021-11-20 12:40:13 +0530

    Merge conflict

--- src/main/java/com/stalin/demo/DemoWorkshopApplication.java
+++ src/main/java/com/stalin/demo/DemoWorkshopApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoWorkshopApplication {
 // Test 1
+   // test
 	public static void main(String[] args) {
 		SpringApplication.run(DemoWorkshopApplication.class, args);
 	}
