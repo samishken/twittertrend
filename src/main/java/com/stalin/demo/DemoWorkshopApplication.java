add75a15a9a21c84325cda316d820978c1645d70
223016861 <stalin.mahapatra@ge.com>
2022-03-10 16:19:35 +0530

    Fixed compilation issue

--- src/main/java/com/stalin/demo/DemoWorkshopApplication.java
+++ src/main/java/com/stalin/demo/DemoWorkshopApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoWorkshopApplication
+public class DemoWorkshopApplication{
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoWorkshopApplication.class, args);
